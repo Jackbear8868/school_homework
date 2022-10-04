583dce7bf0f91a51eee7d126831a9eca7dfa0a18
Jackbear8868 <111693712+Jackbear8868@users.noreply.github.com>
2022-10-04 22:58:14 +0800

    Create 計程hw1_2.py

---
+++ 計程hw1_2.py
@@ -0,0 +1,19 @@
+n = input("IPv4 network prefix length:")
+if n.isdigit() and 32>=int(n)>=0:
+    n = int(n)
+    binary_code = "1"*n + "0"*(32-n)
+    binary_code = int(binary_code)
+    print(f"binary:{binary_code}")
+    ip = [0,0,0,0]
+    for i in range(4):
+        ip[i] = binary_code//(10**(8*(3-i)))
+        binary_code = binary_code - ip[i]*(10**(8*(3-i)))
+        total = 0
+        x = ip[i]
+        for j in range(8):
+            total += (x%10)*(2**j)
+            x = x//10
+        ip[i] = total
+    print(f"subnet mask:{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}")
+else:
+    print("please input number between 0~32")
